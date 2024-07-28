d87285a6b0560838849470f5c2a4fe242e73607a
Julian <juliansmart1996@gmail.com>
2024-07-28 18:56:30 +0900

    hamburger menu working in mobile mode

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const accordionButtons = document.querySelectorAll(".accordion-button");
-  
-    accordionButtons.forEach(button => {
-      button.addEventListener("click", function() {
-        const content = button.nextElementSibling;
-        const icon = button.querySelector(".icon");
-  
-        if (content.style.maxHeight) {
-          content.style.maxHeight = null;
-          icon.textContent = "+";
-          button.appendChild(icon);
-        } else {
-          content.style.maxHeight = content.scrollHeight + "px";
-          icon.textContent = "-";
-          button.appendChild(icon);
-        }
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionButtons = document.querySelectorAll(".accordion-button");
+
+  accordionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const content = button.nextElementSibling;
+      const icon = button.querySelector(".icon");
+
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        icon.textContent = "+";
+        button.appendChild(icon);
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        icon.textContent = "-";
+        button.appendChild(icon);
+      }
     });
   });
+});
 
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const menu = document.getElementById('menu');
-  
-    hamburger.addEventListener('click', () => {
-      menu.classList.toggle('active');
-      hamburger.classList.toggle('open');
-    });
-  });
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
